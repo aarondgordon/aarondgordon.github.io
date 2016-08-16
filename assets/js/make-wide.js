0f869a9d39809294de982132337e78835bd77661
Aaron Gordon <adgordon@live.com>
2016-08-15 22:11:44 -0700

    Insert div tags before and after widened element

--- assets/js/make-wide.js
+++ assets/js/make-wide.js
@@ -1,6 +1,10 @@
 $(function() {
-  $(".is-wide").unwrap();
-  $(".is-wide").wrap("<div class='wide-container'></div>");
-  $("img.is-wide").wrap("<div class='wide-img-container'></div>").addClass("wide-img");
-  alert("Wrapped!");
+  $("p > .is-wide").unwrap();
+  $(".is-wide")
+      .wrap("<div class='wide-container'></div>")
+      .before("</div>")
+      .after("<div class='container'");
+  $("img.is-wide")
+      .wrap("<div class='wide-img-container'></div>")
+      .addClass("wide-img");
 });

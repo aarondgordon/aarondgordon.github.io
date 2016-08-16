358edd9a95f5f6c79e50081fd469505c4a3952a4
Aaron Gordon <adgordon@live.com>
2016-08-15 23:05:11 -0700

    Commenting and cleanup

--- assets/js/make-wide.js
+++ assets/js/make-wide.js
@@ -1,23 +1,31 @@
 $(function() {
+  // Unwrap wide elements from paragraphs added by Jekyll
   $("p > .is-wide").unwrap();
   
-  splitWideElements($("body"));
-      
+  // Grow up wide elements above div container
+  growUpWideElements($("body"));
+  
+  // Wrap wide elements in their own container
+  $(".is-wide").wrap("<div class='wide-container'></div>");
+  
+  // Wrap wide image elements in a second container
   $("img.is-wide")
       .wrap("<div class='wide-img-container'></div>")
       .addClass("wide-img");
 });
 
-function splitWideElements(container) {
+// Moves wide elements in the provided containing element up
+// a level in the DOM, splitting the containing element at each
+// wide element in the process to maintain page flow
+function growUpWideElements(container) {
   var wideE = container.find(".is-wide").filter(":first");
   
   if (wideE.length > 0) {
     var above = wideE.prevAll();
     var below = wideE.nextAll();
     wideE.unwrap();
-    above.wrap("<div class='container'></div>");
-    below.wrap("<div class='container'></div>");
-    wideE.wrap("<div class='wide-container'></div>");
-    splitWideElements(below);
+    above.wrapAll("<div class='container'></div>");
+    below.wrapAll("<div class='container'></div>");\
+    growUpWideElements(below);
   }
 }

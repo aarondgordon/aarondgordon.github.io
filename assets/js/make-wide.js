38a784b6953100a014319e796a54eda9ecee1612
Aaron Gordon <adgordon@live.com>
2016-08-15 22:08:38 -0700

    Wide-img class added to imgs

--- assets/js/make-wide.js
+++ assets/js/make-wide.js
@@ -1,6 +1,6 @@
 $(function() {
-  $("p > .is-wide").unwrap();
+  $(".is-wide").unwrap();
   $(".is-wide").wrap("<div class='wide-container'></div>");
-  $("img.is-wide").wrap("<div class='wide-img-container'></div>");
+  $("img.is-wide").wrap("<div class='wide-img-container'></div>").addClass("wide-img");
   alert("Wrapped!");
 });

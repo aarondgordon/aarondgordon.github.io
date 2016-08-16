e1794b164f42be6cc2d404b645971903a43a4da3
Aaron Gordon <adgordon@live.com>
2016-08-15 22:43:24 -0700

    Update make-wide.js

--- assets/js/make-wide.js
+++ assets/js/make-wide.js
@@ -1,12 +1,23 @@
 $(function() {
   $("p > .is-wide").unwrap();
   
-  $(".is-wide").wrap("<div class='wide-container'></div>");
-  $(".wide-container")
-      .before("</div>")
-      .after("<div class='container'>");
+  splitWideElements($("body"));
       
   $("img.is-wide")
       .wrap("<div class='wide-img-container'></div>")
       .addClass("wide-img");
 });
+
+function splitWideElements(container) {
+  var wideE = container.find(".is-wide:first-child");
+  
+  if (wideE.length > 0) {
+    var above = wideElement.prevAll();
+    var below = wideElement.nextAll();
+    wideE.unwrap();
+    above.wrap("<div class='container'></div>");
+    below.wrap("div class='container'></div>");
+    wideE.wrap("<div class='wide-container'></div>");
+    splitWideElements(below);
+  }
+}
